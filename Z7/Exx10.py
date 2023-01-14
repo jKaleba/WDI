4f95a9524ac41864847b57343dc2386977aa1e1b
Jan Kalęba <93594746+WegielWBucie@users.noreply.github.com>
2023-01-14 20:21:52 +0100

    >> done

--- Z7/Exx10.py
+++ Z7/Exx10.py
@@ -1,3 +1,4 @@
+import time
 from random import randint
 
 
@@ -8,74 +9,100 @@ class Node:
         self.next = next
 
 
-class Number:
-    pointer: Node
+def reverse(p: Node):
+    q = p
 
-    def __init__(self):
-        self.pointer = Node(None)
+    def reverseR(pointer: Node, previous=None):
 
-    def getGuardian(self):
-        return self.pointer
+        nonlocal q
 
-    def setGuardianNext(self, next):
-        self.pointer = next
+        if pointer.next is not None:
+            reverseR(pointer.next, pointer)
 
-    def print(self, message=None):
+        if pointer.next is None and previous is not None:
 
-        print(message, end="\t")
+            if q == p:
+                q = pointer
 
-        g = self.pointer.next if self.pointer.value is None else self.pointer
-        print("Guardian -> ", end="")
-        while g is not None:
-            print(g.value, end=" -> ")
-            g = g.next
-        print(g)
+            previous.next = None
+            pointer.next = previous
 
-    def appendDigit(self, newValue, pointer=None):
-
-        if pointer is None:
-            pointer = self.pointer
-
-        if pointer.next is None:
-            newNode = Node(newValue)
-            pointer.next = newNode
-            return
-
-        self.appendDigit(newValue, pointer.next)
-
-    def insert(self, newValue):
-
-        newNode = Node(newValue, self.pointer.next)
-        self.pointer.next = newNode
-        return
-
-    @staticmethod
-    def add(number1, number2):
-
-        digit1 = number1.getGuardian()
-        digit2 = number2.getGuardian()
-
-        def addR(digitPointer1, digitPointer2):
-            return
-        
-        
-            
+    reverseR(p)
+    return q
 
 
-freeLine = lambda: print("---")
+def add(p: Node, q: Node):
+    p = reverse(p)
+    q = reverse(q)
+
+    guardian = Node(-1)
+    current = guardian
+    rest = 0
+    while True:
+
+        sum = Node(p.value + q.value + rest)
+        rest = sum.value // 10
+        sum.value %= 10
+
+        current.next = sum
+        current = current.next
+
+        p = p.next
+        q = q.next
+
+        if p is None and q is None:
+            break
+
+        if p is None:
+            p = Node(0)
+
+        if q is None:
+            q = Node(0)
+
+    if rest > 0:
+        sum = Node(rest)
+        current.next = sum
+
+    return reverse(guardian.next)
+
 
 if __name__ == '__main__':
 
-    num1, num2 = Number(), Number()
-    
+    freeLine = lambda: print("-----")
 
-    for i in range(randint(3, 5)):
-        num1.appendDigit(randint(1, 9))
+    first1 = Node(randint(1, 9))
+    first2 = Node(randint(1, 9))
+    current1 = first1
+    current2 = first2
+    for i in range(randint(5, 10)):
+        next1 = Node(randint(0, 9))
 
-    for i in range(randint(1, 3)):
-        num2.appendDigit(randint(1, 9))
+        current1.next = next1
+        current1 = current1.next
 
-    mSum: Number = Number.add(num1, num2)
+    for i in range(randint(5, 10)):
+        next2 = Node(randint(0, 9))
 
+        current2.next = next2
+        current2 = current2.next
 
-    mSum.print("Sum equals:")
+    current1 = first1
+    current2 = first2
+    while current1 is not None:
+        print(current1.value, end=" ")
+        current1 = current1.next
+    print()
+
+    while current2 is not None:
+        print(current2.value, end=" ")
+        current2 = current2.next
+    print()
+
+    freeLine()
+
+    current3 = add(first1, first2)
+
+    while current3 is not None:
+        print(current3.value, end=" ")
+        current3 = current3.next
+    print()
