aa444e2d3827b408ac3e8d3f8e5d778374f54819
Jan Kalęba <93594746+WegielWBucie@users.noreply.github.com>
2023-01-17 04:47:14 +0100

    >> basic idea

---
+++ Z7/geometricSequence.py
@@ -0,0 +1,93 @@
+from random import randint
+
+
+class Node:
+
+    def __init__(self, val, next=None):
+        self.val = val
+        self.next = None
+
+
+def reversedList(pointer: Node):
+    return pointer
+
+
+def repair(pointer: Node, divisor=1):
+    beginning = pointer
+
+    NWD = lambda a, b: NWD(b, a % b) if b else a
+    quotient = pointer.next.val / pointer.val
+
+    signChange = False
+
+    while pointer.next is not None:
+
+        currQ = pointer.next.val / pointer.val
+
+        if 0 < currQ % 1 < 1:
+            return repair(reversedList(pointer), 1 // currQ)
+
+        quotient = NWD(quotient, currQ)
+
+        if currQ < 0:
+            signChange = True
+            quotient = quotient // 2
+
+        pointer = pointer.next
+
+    pointer = beginning
+    newElements = 0
+
+    while pointer.next is not None:
+        if pointer.next.val // pointer.val != quotient:
+
+            newNode = Node(int(pointer.val * quotient)) if quotient % 1 == 0 else Node(pointer.val * quotient)
+            newNode.next, pointer.next = pointer.next, newNode
+            newElements += 1
+
+        else:
+            pointer = pointer.next
+
+    return newElements
+
+
+if __name__ == '__main__':
+
+    asd = 0
+
+    randRange = (1, 5, 1)
+
+    pt = Node(1)
+    curr = pt
+
+    new = Node(2 ** 2)
+    curr.next = new
+    curr = curr.next
+
+    new = Node(2 ** 3)
+    curr.next = new
+    curr = curr.next
+
+    new = Node(2 ** 5)
+    curr.next = new
+    curr = curr.next
+
+    new = Node(2 ** 6)
+    curr.next = new
+    curr = curr.next
+
+    curr = pt
+    while curr is not None:
+        print(curr.val, end=" ")
+        curr = curr.next
+    print()
+
+    elements = repair(pt, True)
+
+    curr = pt
+    while curr is not None:
+        print(curr.val, end=" ")
+        curr = curr.next
+    print()
+
+    print(elements)
